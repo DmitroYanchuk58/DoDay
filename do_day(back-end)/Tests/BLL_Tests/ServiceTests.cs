7387becf2aaf51431a773e2ea21e86a9256694ab
Dmitro Yanchuk <dimochkavyan@gmail.com>
2026-03-17 19:42:11 +0200

    Make abstract class for all services tests that have GetDbContext() method

---
+++ do_day(back-end)/Tests/BLL_Tests/ServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using Data_Access_Layer.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tests.BLL_Tests
+{
+    public abstract class ServiceTests
+    {
+        protected DoDayDBContext GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<DoDayDBContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            return new DoDayDBContext(options);
+        }
+    }
+}
