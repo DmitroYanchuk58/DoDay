691161b1afeedaf872ab6160798adc68e06f04b0
Dmitro Yanchuk <dimochkavyan@gmail.com>
2026-03-17 22:36:22 +0200

    Make CreateCategoryOption()

---
+++ do_day(back-end)/Business Logic Layer/Services/CategoryOptionService.cs
@@ -0,0 +1,33 @@
+﻿using Business_Logic_Layer.DTO;
+using Data_Access_Layer.DatabaseContext;
+using Data_Access_Layer.Entities;
+using Data_Access_Layer.Repositories;
+using Task = System.Threading.Tasks.Task;
+
+namespace Business_Logic_Layer.Services
+{
+    public class CategoryOptionService
+    {
+        CrudRepository<CategoryOption> _categoryOptionRepository;
+
+        public CategoryOptionService(DoDayDBContext context)
+        {
+            _categoryOptionRepository = new CrudRepository<CategoryOption>(context);
+        }
+
+        public async Task CreateCategoryOption(CategoryOptionDTO categoryOptionDTO)
+        {
+            if (categoryOptionDTO == null)
+            {
+                throw new ArgumentNullException(nameof(categoryOptionDTO), "CategoryOptionDTO cannot be null.");
+            }
+            CategoryOption categoryOption = new CategoryOption() 
+            { 
+                Id = categoryOptionDTO.Id,
+                Key = categoryOptionDTO.Key,
+                Value = categoryOptionDTO.Value
+            };
+            await _categoryOptionRepository.CreateAsync(categoryOption);
+        }
+    }
+}
