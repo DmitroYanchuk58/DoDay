a4e9bcc716ac24bcd283f594c801a3997d21cbd0
Dmitro Yanchuk <dimochkavyan@gmail.com>
2026-04-10 14:02:34 +0300

    Make controller for category option

---
+++ do_day(back-end)/DoDay/Controllers/CategoryOptionService.cs
@@ -0,0 +1,19 @@
+﻿using Business_Logic_Layer.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Layer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryOptionService : ControllerBase
+    {
+        public ICategoryOptionService _service;
+
+        public CategoryOptionService(ICategoryOptionService service)
+        {
+            _service = service;
+        }
+
+
+    }
+}
