0317fb08af9b615c7192bbc533f9856cfbe333cc
Dmitro Yanchuk <dimochkavyan@gmail.com>
2026-04-10 14:26:16 +0300

    Make repository for join loading category with category options

---
+++ do_day(back-end)/Data Access Layer/Repositories/IJoin.cs
@@ -0,0 +1,9 @@
+﻿using Data_Access_Layer.Entities;
+
+namespace Data_Access_Layer.Repositories
+{
+    public interface IJoin
+    {
+        public Task<Category> GetCategoryWithOptionsAsync(Guid id);
+    }
+}
---
+++ do_day(back-end)/Data Access Layer/Repositories/JoinRepository.cs
@@ -0,0 +1,28 @@
+﻿using Data_Access_Layer.DatabaseContext;
+using Data_Access_Layer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access_Layer.Repositories
+{
+    public class JoinRepository
+    {
+        private readonly DoDayDBContext _context;
+
+        public JoinRepository(DoDayDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Category> GetCategoryWithOptionsAsync(Guid id)
+        {
+            return await _context.Categories
+                .Include(c => c.CategoryOptions) 
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+    }
+}
